f146cbf6386d291a441e819e45e8338e6f9c86d7
yodaTahirShaikh <tahir.shaikh@gupshup.io>
2024-07-01 15:31:09 +0530

    Refactor: Enhance string calculator to handle multiple delimiters, negative numbers, and large numbers

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,34 @@
 function stringCalculator(input) {
     if (input === '') return 0;
-    const numbers = input.split(',').map(Number);
-    return numbers.reduce((sum, num) => sum + num, 0);
+
+    // Default delimiters
+    let delimiters = [',', '\n'];
+    let numbersString = input;
+
+    // Check for custom delimiter
+    if (input.startsWith('//')) {
+        const delimiterEndIndex = input.indexOf('\n');
+        const delimiter = input.substring(2, delimiterEndIndex);
+        delimiters.push(delimiter);
+        numbersString = input.substring(delimiterEndIndex + 1);
+    }
+
+    // Split the numbers by the delimiters
+    const numbers = numbersString.split(new RegExp(`[${delimiters.join('')}]`)).map(Number);
+
+    // Check negative numbers
+    const negatives = numbers.filter(num => num < 0);
+    if (negatives.length > 0) {
+        throw new Error(`negatives not allowed: ${negatives.join(', ')}`);
+    }
+
+    // Sum numbers, ignoring those greater than 1000
+    return numbers.reduce((sum, num) => {
+        if (num <= 1000) {
+            return sum + num;
+        }
+        return sum;
+    }, 0);
 }
 
 module.exports = stringCalculator;
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -12,3 +12,24 @@ test('returns the number itself for a single number', () => {
 test('returns the sum of two numbers', () => {
     expect(stringCalculator('1,2')).toBe(3);
 });
+
+test('returns the sum of numbers with new lines', () => {
+    expect(stringCalculator('1\n2,3')).toBe(6);
+});
+
+test('throws an error for negative numbers', () => {
+    expect(() => stringCalculator('1,-2,3')).toThrow('negatives not allowed: -2');
+});
+
+test('ignores numbers greater than 1000', () => {
+    expect(stringCalculator('2,1001')).toBe(2);
+});
+
+test('supports custom delimiters', () => {
+    expect(stringCalculator('//;\n1;2')).toBe(3);
+    expect(stringCalculator('//#\n1#2#3')).toBe(6);
+});
+
+test('supports multiple delimiters', () => {
+    expect(stringCalculator('//;\n1;2\n3,4')).toBe(10);
+});
